84788ec335db3b8c69a861377f099b07984170ce
gaolou1991 <zsqiang91@qq.com>
2019-06-11 00:38:52 +0800

    插入排序实现

---
+++ sort/install.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+#define TEST
+
+//
+// 插入排序
+//
+
+void install_sort(int array[], int len) {
+
+}
+
+
+//
+// 测试
+//
+#ifdef TEST
+int main(void) {
+
+    return 0;
+}
+#endif
