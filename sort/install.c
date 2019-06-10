f1d1695d6a88a9bafb1656acff74c386615cb6d1
gaolou1991 <zsqiang91@qq.com>
2019-06-11 00:39:05 +0800

    插入排序实现

--- sort/install.c
+++ sort/install.c
@@ -8,7 +8,24 @@
 //
 
 void install_sort(int array[], int len) {
+    int pos, index;
 
+    // 异常情况处理
+    if(len <= 1 || array == NULL) return;
+
+
+    // 正常排序
+    // 插入排序要求P位置之前的都是有序的
+    // 因此，只需要在P位置之前找到一个合适位置即可
+    for(pos=1; pos<len; pos++) {
+        for(index=pos; index>0; index--) {
+            if(array[index] < array[index-1]) {
+                array[index] = array[index-1];
+            }
+            else break;
+        }
+        array[index] = array[pos];
+    }
 }
 
 
